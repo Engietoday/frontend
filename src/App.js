1ff4209a5c6afde476504f72172b18e582c3c190
Ao Yu <engietoday@gmail.com>
2022-07-30 20:33:49 -0400

    UPdate

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello Frontend!
+          Hello Aliciah Hill, we have been expecting you.
         </p>
         <a
           className="App-link"
